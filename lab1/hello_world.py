ac79e0513eca83bca1cc34ad1f41bec673a8f744
Magzhan Baishuakov <magzhanbaishuakov@MacBook-Air-Magzhan.local>
2026-01-26 02:29:02 +0500

    Add lab1 folder with document

---
+++ lab1/hello_world.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+
+import sys
+print(sys.version)
+
+if 5 > 2:
+    print("5 is bigger")
+
+print("Practise makes perfect")
+
+print("How?"); print("are"); print("you")
+
+print('Leo', end = " ")
+print("Messi")
