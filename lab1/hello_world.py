106287ac94fece0a4917ff6d2258bb84fab95061
Magzhan Baishuakov <magzhanbaishuakov@MacBook-Air-Magzhan.local>
2026-01-26 02:35:09 +0500

    Add lab1 as normal folder

--- lab1/hello_world.py
+++ lab1/hello_world.py
@@ -1,14 +1,12 @@
 print("Hello, World!")
 
-import sys
-print(sys.version)
-
 if 5 > 2:
     print("5 is bigger")
 
-print("Practise makes perfect")
-
-print("How?"); print("are"); print("you")
-
 print('Leo', end = " ")
 print("Messi")
+
+print(2 * 3)
+
+#this is a comment
+#print("727")
